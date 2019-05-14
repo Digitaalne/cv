66108d4da3f7e8bf8f79d901d5f5b5bb3f48d213
Digitaalne <jarekx.j@gmail.com>
2019-05-14 23:56:19 +0300

    background added

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  /*https://coolbackgrounds.io/*/
+  background-image: url('./assets/cool-background.png');
+  background-size: cover;
 }
 </style>
